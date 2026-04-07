11e8f7982a6dca34c4c0710069f1ddde0f61e444
Kajal <kajalsingh.tech10@gmail.com>
2026-04-07 22:34:58 +0530

    Day 1: Completed Operators + Conditionals notes

---
+++ 01_python_basics/day01...hello.py
@@ -0,0 +1,93 @@
+#Operators 
+# 1.Arithmetic operators - a.Addition
+a=5 
+b=10
+print(a+b)  #15
+
+# 1.Arithmetic operators - b.subtraction
+a=15 
+b=10
+print(a-b)  #5
+
+# 1.Arithmetic operators - c.multiplication 
+a=15 
+b=10
+print(a*b)  #150
+
+# 1.Arithmetic operators - d.division
+a=15 
+b=10
+print(a/b)  #1.5
+
+# 1.Arithmetic operators - e.floor division
+a=15 
+b=10
+print(a//b)  #1
+
+# 1.Arithmetic operators - f.mod
+a=15 
+b=10
+print(a%b)  #5
+
+# 1.Arithmetic operators - g.power
+a=15 
+b=10
+print(a**2)  #225
+
+# 2. Comparison Operators (Return True or False)
+a = 15
+b = 10
+
+print(a > b)     # True
+print(a < b)     # False
+print(a >= b)    # True
+print(a <= b)    # False
+print(a == b)    # False   
+print(a != b)    # True
+
+
+# 3. Logical operators 
+x = True
+y = False
+
+print(x and y)   # False   
+print(x or y)    # True    
+print(not x)     # False
+print(not y)     # True
+
+# if, elif and else 
+print("Example 1")
+age=19
+if age>= 18:
+ print("You are eligible for driving license")
+else: 
+ print("You are not eligible for driving license")
+
+ print("Example 2")
+ #odd and even
+num=234
+
+if num % 2==0:
+ print("Even")
+elif num==1:
+ print("number is neither odd nor even")
+else:
+ print("Number is odd")
+
+
+# Summary of Day 1 Topics
+"""
+Topics Covered on Day 1:
+1. Arithmetic Operators (+, -, *, /, //, %, **)
+2. Comparison Operators (>, <, >=, <=, ==, !=)
+3. Logical Operators (and, or, not)
+4. Conditional Statements (if, elif, else)
+
+Next Topics (Day 2 & 3):
+→ Loops (for and while)
+→ Functions
+"""
+
+print("Day 1 completed successfully!")
+print("I have understood Operators and Conditionals.")
+print("Tomorrow I will learn Loops.")
