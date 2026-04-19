9eb90b7795c1c0b185a3aaa2b1dc9e1618140329
Kajal <kajalsingh.tech10@gmail.com>
2026-04-19 11:57:23 +0530

    Day 1 - CycleFit project started, user profile dictionary

---
+++ projects/CycleFit/user_profile.py
@@ -0,0 +1,9 @@
+user_data={
+    "name":"Kajal",
+    "age":19,
+    "weight": 68.8,
+    "height": 157,
+    "activity_level":"moderate-4x a week",
+    "recent_period":"28/03/2026",
+    "diet":"Pure veg"
+}
